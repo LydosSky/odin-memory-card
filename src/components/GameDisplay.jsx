0da587629b71841637632e92b7f2b49da127f064
Lydos <lydossky@gmail.com>
2024-12-05 21:06:11 +0300

    feat: now you can set number of cards

--- src/components/GameDisplay.jsx
+++ src/components/GameDisplay.jsx
@@ -1,3 +1,23 @@
-export default function GameDisplay() {
-  return <h1>GAME DISPLAY</h1>;
+export default function GameDisplay({ numberOfCards, setNumberOfCards }) {
+  const handleClick = function () {
+    const input = document.querySelector('#numberOfCards');
+    setNumberOfCards(input.value);
+  };
+
+  return (
+    <div className="display-container">
+      <p>Playing with {numberOfCards}</p>
+      <input
+        id="numberOfCards"
+        name="number"
+        type="number"
+        defaultValue={numberOfCards}
+        min="0"
+        max="52"
+      />
+      <button type="submit" onClick={handleClick}>
+        Set
+      </button>
+    </div>
+  );
 }
