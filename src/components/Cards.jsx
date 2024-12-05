ded98ada7c3b8a6db05f7b34e23ae95cadf85e6e
Lydos <lydossky@gmail.com>
2024-12-05 20:43:38 +0300

    refactor: grid column number based on number pics

--- src/components/Cards.jsx
+++ src/components/Cards.jsx
@@ -1,9 +1,12 @@
 import Card from './Card';
 import '../styles/cards.css';
 
-export default function Cards({ cards }) {
+export default function Cards({ cards, numberOfCards }) {
   return (
-    <div className="cards-container">
+    <div
+      style={{ gridTemplateColumns: `repeat(${numberOfCards / 2}, 1fr)` }}
+      className="cards-container"
+    >
       {cards.map((card) => (
         <Card key={crypto.randomUUID()} card={card} />
       ))}
